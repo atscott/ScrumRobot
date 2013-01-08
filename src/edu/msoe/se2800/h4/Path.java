e78b7c48c93939d21dcd675eaa1aa9f020db6166
Andrew Scott <atscott01@gmail.com>
2013-01-08 13:20:50 -0600

    completed read from file

--- src/edu/msoe/se2800/h4/Path.java
+++ src/edu/msoe/se2800/h4/Path.java
@@ -3,39 +3,97 @@ package edu.msoe.se2800.h4;
 import com.google.common.io.Closeables;
 
 import java.awt.Point;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public enum Path {
-    
+
     INSTANCE;
 
-	private List<Point> points = new ArrayList<Point>();
-	
-	public void writeToFile(File outputFile) throws IOException {
-	    boolean threwException = true;
-	    Writer writer = null;	    
-	    try {
-	        writer = new BufferedWriter(new FileWriter(outputFile));
-	        for (Point point : points) {
-	            
-	        }
-	        threwException = false;
+    private List<Point> points = new ArrayList<Point>();
+
+    public void writeToFile(File outputFile) throws IOException {
+        boolean threwException = true;
+        Writer writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(outputFile));
+            for (Point point : points) {
+
+            }
+            threwException = false;
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } finally {
-	        Closeables.close(writer, threwException);
-	    }
-	}
-	
-	public void readFromFile(File input) {
-	    
-	}
+            Closeables.close(writer, threwException);
+        }
+    }
+
+    /**
+     * Parses the given file for points and assigns those points to the points variable. If there is an error while
+     * parsing or the file or the file could not be found, an exception is thrown and the points variable remains
+     * as it was before entering this method.
+     *
+     * @param input The file containing the coordinates
+     * @throws UnsupportedEncodingException Thrown if there was an error while parsing because of bad file format
+     * @throws FileNotFoundException        Thrown if the file does not exist
+     */
+    public void readFromFile(File input) throws UnsupportedEncodingException, FileNotFoundException {
+        if (input == null) {
+            throw new NullPointerException();
+        } else if (!input.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        List<Point> tempPoints = new ArrayList<Point>();
+
+        try {
+            // set up the reader variables
+            FileInputStream fs = new FileInputStream(input);
+            DataInputStream in = new DataInputStream(fs);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            int lineCount = 1;
+            boolean error = false;
+            // while there are still lines to parse
+            while ((line = br.readLine()) != null) {
+                // verify that the first line is the predefined header
+                if (lineCount == 1) {
+                    if (!line.equals("some defined header")) {
+                        error = true;
+                    }
+
+                } else {
+                    // not on the first line so parse the coordinates. x and y separated by a ,
+                    String[] coordinatesAsString = line.split(",");
+                    if (coordinatesAsString.length != 2) {
+                        error = true;
+                    } else {
+                        try {
+                            int x = Integer.parseInt(coordinatesAsString[0]);
+                            int y = Integer.parseInt(coordinatesAsString[1]);
+                            tempPoints.add(new Point(x, y));
+                        } catch (Exception e) {
+                            error = true;
+                        }
+
+                    }
+
+                }
+
+                if (error) {
+                    throw new UnsupportedEncodingException("File has corrupt data. Line " + lineCount + " was not in the correct format");
+                }
+
+                lineCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        points = tempPoints;
+    }
 
 }
