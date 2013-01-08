56b2943e458a7ddeb2c3ae758ec2fb5f334bc99c
Marius Volkhart <Marius@volkhart.com>
2013-01-08 14:08:48 -0600

    Added ability to write Path to file & appropriate tests

--- src/edu/msoe/se2800/h4/Path.java
+++ src/edu/msoe/se2800/h4/Path.java
@@ -1,41 +1,50 @@
+
 package edu.msoe.se2800.h4;
 
-import com.google.common.io.Closeables;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.awt.Point;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public enum Path {
-    
+
     INSTANCE;
 
-	private List<Point> points = new ArrayList<Point>();
-	
-	public void writeToFile(File outputFile) throws IOException {
-	    boolean threwException = true;
-	    Writer writer = null;	    
-	    try {
-	        writer = new BufferedWriter(new FileWriter(outputFile));
-	        for (Point point : points) {
-	            
-	        }
-	        threwException = false;
+    private List<Point> points = new ArrayList<Point>();
+
+    public boolean writeToFile(File outputFile) {
+        checkNotNull(outputFile, "The File instance passed was null");
+        boolean threwException = true;
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outputFile);
+            writer.println("## The points in this file are represented as <x-value, y-value>. ##");
+            for (Point point : points) {
+                writer.println(point.getX());
+                writer.println(point.getY());
+            }
+            threwException = false;
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } finally {
-	        Closeables.close(writer, threwException);
-	    }
-	}
-	
-	public void readFromFile(File input) {
-	    
-	}
+            if (writer != null) {
+                writer.close();
+            }
+        }
+        return threwException;
+    }
+
+    public void readFromFile(File input) {
+
+    }
+    
+    public void reset() {
+        points.clear();
+    }
 
 }
