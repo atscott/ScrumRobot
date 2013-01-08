64b1ce8558a7edb2bff4b18906b8e7128f8fe2a2
Marius Volkhart <Marius@volkhart.com>
2013-01-08 11:45:01 -0600

    Changed Path to Enum

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ScrumRobot
 ==========
 
-Scrum class to produce robot program
+Scrum class at Milwaukee School of Engineering to produce a robot based on Lego NXT that is able to monitor a hallway, has user access control and provide easy programming interfaces.
+
+The Lejos libraries at the time of this writing are licensed under Mozilla Public License 1.0. Refer to http://sourceforge.net/apps/mediawiki/lejos/index.php?title=Licensing_Issues for more information.
 
 Requires a 32 bit jdk to be used as a result of the 32 bit Lejos libraries.
--- src/edu/msoe/se2800/h4/Path.java
+++ src/edu/msoe/se2800/h4/Path.java
@@ -1,13 +1,41 @@
 package edu.msoe.se2800.h4;
 
-public class Path {
+import com.google.common.io.Closeables;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.awt.Point;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
+public enum Path {
+    
+    INSTANCE;
+
+	private List<Point> points = new ArrayList<Point>();
+	
+	public void writeToFile(File outputFile) throws IOException {
+	    boolean threwException = true;
+	    Writer writer = null;	    
+	    try {
+	        writer = new BufferedWriter(new FileWriter(outputFile));
+	        for (Point point : points) {
+	            
+	        }
+	        threwException = false;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+	        Closeables.close(writer, threwException);
+	    }
+	}
+	
+	public void readFromFile(File input) {
+	    
 	}
 
 }
