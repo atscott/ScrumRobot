f7d408096b892d15b5ee3690f709b57b6b54b9b0
Brewcrewfan10 <koenigj@msoe.edu>
2013-01-15 11:07:08 -0600

    Missed deleting some unnecessary imports.

--- src/edu/msoe/se2800/h4/FileIO.java
+++ src/edu/msoe/se2800/h4/FileIO.java
@@ -1,17 +1,10 @@
 package edu.msoe.se2800.h4;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-
-import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
