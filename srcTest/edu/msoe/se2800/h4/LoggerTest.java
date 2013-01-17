931b0c8f553714d1a4e1489ef4e9508b2057baf1
Marius Volkhart <Marius@volkhart.com>
2013-01-17 03:07:32 -0600

    Test & repo cleanup
    - moved the database to the resources folder to match folder structure
    - Modified Path tests. See inline comments.
    - Separated some of the Path tests into separate classes because the
    @BeforeMethod call made some tests obsolete. We need to watch out for
    this
    - Modified Logger tests. See inline comments.

--- srcTest/edu/msoe/se2800/h4/LoggerTest.java
+++ srcTest/edu/msoe/se2800/h4/LoggerTest.java
@@ -1,7 +1,7 @@
+
 package edu.msoe.se2800.h4;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -9,59 +9,47 @@ import java.io.File;
 import java.util.Scanner;
 
 /**
- * Created with IntelliJ IDEA.
- * User: tohtzk
- * Date: 1/15/13
- * Time: 10:19 AM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: tohtzk Date: 1/15/13 Time: 10:19 AM To change this template use
+ * File | Settings | File Templates.
  */
 public class LoggerTest {
     private Logger logs;
 
     @BeforeMethod
-    public void setupLogger(){
+    public void setupLogger() {
         logs = Logger.INSTANCE;
     }
 
-    /**
-     * Testing for attempting to print with a null logger
-     * expecting null pointer exception
-     */
-    @Test(expectedExceptions = NullPointerException.class)
-    public void nullLogger(){
+    @Test(expectedExceptions = NullPointerException.class, description = "Testing for attempting to print with a null logger expecting null pointer exception")
+    public void nullLogger() {
         logs = null;
-        logs.log("NullTest","NULLPRINT");
+        logs.log("NullTest", "NULLPRINT");
+        // FIXME @Kevin this test doesn't test any functionality.
     }
 
-    /**
-     * Testing for the existance of the output.txt
-     */
-    @Test
-    public void validLogOutputFile(){
-        logs.log("testing","testingprint");
+    @Test(description = "Testing for the existence of the output.txt")
+    public void validLogOutputFile() {
+        logs.log("testing", "testingprint");
         File f = new File("output.log");
         Assert.assertTrue(f.exists());
+        // FIXME @Kevin make sure to delete the file at the start of this test. otherwise you don't
+        // know if it was there already or not
     }
 
-    /**
-     * Testing that the given phrase was parsed correctly
-     */
-    @Test
-    public void validPrintInFile(){
-        try{
-            logs.log("testing","testingprint");
+    @Test(description = "Testing that the given phrase was parsed correctly")
+    public void validPrintInFile() {
+        // TODO @Kevin don't catch the exception. 2 reasons for this. 1. we don't see calls to
+        // system.out in the logs. you see them in the console but not in html. 2. if an exception
+        // occurs, throw it because the test will fail then which is the behavior we want
+        try {
+            logs.log("testing", "testingprint");
             File f = new File("output.log");
             Scanner scan = new Scanner(f);
             String s = scan.next();
-            Assert.assertEquals(s,"testingprint");
-        } catch  (Exception e) {
+            Assert.assertEquals(s, "testingprint");
+        } catch (Exception e) {
             System.out.println("Shouldn't file i/o print");
         }
-    }
-
-    @AfterClass
-    public void finish(){
-        logs = null;
     }
 
 }
--- srcTest/edu/msoe/se2800/h4/PathTest.java
+++ srcTest/edu/msoe/se2800/h4/PathTest.java
@@ -1,27 +1,15 @@
+//TODO consider getting rid of these tests. our implementation just calls the underlying structure so these tests are pretty useless. It might make more sense to just make a inline comment in each method in the Path class to create a test if updating it.
 
 package edu.msoe.se2800.h4;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
-
-import com.google.common.io.Closeables;
-
 import edu.msoe.se2800.h4.jplot.JPoint;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class PathTest {
 
@@ -44,138 +32,36 @@ public class PathTest {
         mPath.reset();
     }
 
-    @Test(expectedExceptions = {
-            NullPointerException.class
-    }, description = "Tests passing a null parameter. Expects a NullPointerException")
-    public void writeToFileNullParameter() {
-        mPath.writeToFile(null);
-    }
-
-    @Test(description = "This is a test writing under 'normal' conditions")
-    public void writeAsExpected() throws IOException {
-        File file = new File("TestFile.scrumbot");
-        file.deleteOnExit();
-        assertTrue(mPath.writeToFile(file));
-
-        // Make sure we have the number of lines we expect
-        int count = lineCount(file);
-        int expectedCount = mPath.size() + 1;
-        assertEquals(count, expectedCount, "Test the line counts");
-    }
-
-    @Test(enabled = false)
-    public void writeToReadOnlyFile() {
-        // TODO Marius: test writing to read only files
-    }
-
-    @Test(description = "Tries to write to a non-existent file")
-    public void writeToNonExistentFile() {
-        assertFalse(mPath.writeToFile(new File("")));
-    }
-
-    @Test(expectedExceptions = {
-            NullPointerException.class
-    })
-    public void testReadNull() throws Path.BadFormatException, FileNotFoundException {
-        mPath.readFromFile(null);
-    }
-
-    @Test(expectedExceptions = {
-            FileNotFoundException.class
-    })
-    public void testNonExistentFile() throws Path.BadFormatException, FileNotFoundException {
-        File f = new File("aaa");
-        mPath.readFromFile(f);
-    }
-    @Test(expectedExceptions = {
-            Path.BadFormatException.class
-    })
-    public void testBadFormatCoordinates() throws Path.BadFormatException, FileNotFoundException {
-        File f = new File("./resourcesTest/BadCoordinates.scrumbot");
-        try {
-            mPath.readFromFile(f);
-        } catch (Path.BadFormatException e) {
-            assertEquals(mPath.size(), 0);
-            throw e;
-        }
-    }
-
-    @Test
-    public void testEmptyPath() throws Path.BadFormatException, FileNotFoundException {
-        File f = new File("./resourcesTest/ExamplePathFileNoCoordinates.scrumbot");
-        mPath.readFromFile(f);
-    }
-
-    @Test
-    public void testMultipleCoordinates() throws Path.BadFormatException, IOException {
-        File f = new File("./resourcesTest/ReadOnlyFile.scrumbot");
-
-        mPath.readFromFile(f);
-        FileReader f2 = new FileReader(f);
-        BufferedReader br = new BufferedReader(f2);
-        int lines = 0;
-        try {
-            while (br.readLine() != null) {
-                lines++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace(); // To change body of catch statement use File | Settings | File
-                                 // Templates.
-        } finally {
-            Closeables.close(br, true);
-        }
-        assertEquals(mPath.size(), lines - 1);
-    }
-
-    /*
-     * This was taken from StackTrace & slightly modified http://stackoverflow.com
-     * /questions/453018/number-of-lines-in-a-file-in-java
-     */
-    private int lineCount(File file) throws IOException {
-        InputStream is = new BufferedInputStream(new FileInputStream(file));
-        try {
-            byte[] c = new byte[1024];
-            int count = 0;
-            int readChars;
-            boolean empty = true;
-            while ((readChars = is.read(c)) != -1) {
-                empty = false;
-                for (int i = 0; i < readChars; ++i) {
-                    if (c[i] == '\n')
-                        ++count;
-                }
-            }
-            return (count == 0 && !empty) ? 1 : count;
-        } finally {
-            is.close();
-        }
-    }
-
-    @Test
+    @Test(description = "")
+    // TODO @Macon add a description
     public void testReset() {
         mPath.add(new JPoint());
         mPath.add(new JPoint());
         mPath.reset();
     }
 
-    @Test
+    @Test(description = "")
+    // TODO @Macon add a description
     public void testAdd() {
         JPoint point1 = new JPoint();
         assertTrue(mPath.add(point1));
     }
 
-    @Test
+    @Test(description = "")
+    // TODO @Macon add a description
     public void testGetNull() {
         assertEquals(null, mPath.get(0));
     }
 
-    @Test
+    @Test(description = "")
+    // TODO @Macon add a description
     public void testGet() {
         JPoint point = new JPoint();
         assertEquals(point, mPath.get(0));
     }
 
-    @Test
+    @Test(description = "")
+    // TODO @Macon add a description
     public void testSize() {
         mPath.add(new JPoint());
         int count = 1;
