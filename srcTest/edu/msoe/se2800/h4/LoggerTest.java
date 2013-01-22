62e0a8c309cdb36b5638ac506d84539c7f3df4ad
kt <ktohtz@gmail.com>
2013-01-21 18:32:58 -0600

    Adjusting Logger tests

--- srcTest/edu/msoe/se2800/h4/LoggerTest.java
+++ srcTest/edu/msoe/se2800/h4/LoggerTest.java
@@ -21,7 +21,12 @@ public class LoggerTest {
 
     @BeforeMethod
     public void setupLogger() {
+
+        //TODO this only needs to happen once. @BeforeClass is what you're looking for.
         logs = Logger.INSTANCE;
+
+        //TODO before each method, we should be "resetting" the environment. This means deleteing any log files that potentially exist.
+        //TODO for convenience i've made the file name for the logger a static variable so get it by using Logger.FILE_NAME in all the tests
     }
 
     /**
@@ -29,6 +34,8 @@ public class LoggerTest {
      */
     @Test
     public void validLogOutputFile() {
+
+        //TODO put the description in a description annotation. doing javadoc style doesnt give the description in the reports. see the WritePathTest for details
         File f = new File("output.log");
         if(f.exists()){
             f.delete();
@@ -43,6 +50,8 @@ public class LoggerTest {
     @Test
     public void validPrintInFile() throws FileNotFoundException {
 
+        //TODO put the description in a description annotation. doing javadoc style doesnt give the description in the reports. see the WritePathTest for details
+
         logs.log("testing", "testingprint");
         File f = new File("output.log");
         Scanner scan = new Scanner(f);
@@ -51,9 +60,15 @@ public class LoggerTest {
 
     }
 
+    //TODO write the tests for the stub method in the logger class. It's documented so you should be able to write the tests for it without knowing the implementation.
+    //TODO i updated the log method javadoc so make sure all that functionality is tested.
+    //TODO test tread safety of Logger
+
     @AfterClass
     public void finish() {
         logs = null;
+
+        //TODO this isn't strictly needed. The reference will go out of scope & will be collected
     }
 
 }
