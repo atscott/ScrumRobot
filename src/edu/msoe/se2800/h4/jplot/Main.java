07bda6d847aa04ee6b45e61e9db6eb7b81a3ea4a
Andrew Scott <atscott01@gmail.com>
2013-01-22 11:51:44 -0600

    logging now works

--- src/edu/msoe/se2800/h4/jplot/Main.java
+++ src/edu/msoe/se2800/h4/jplot/Main.java
@@ -1,11 +1,16 @@
 package edu.msoe.se2800.h4.jplot;
 
 
+import edu.msoe.se2800.h4.AdministrationFeatures.LoginUI;
+
 public class Main {
 
-	public static void main(String[] args) {
-		JPlotController controller = JPlotController.getInstance();
-		controller.init();
-	}
-	
+    public static void main(String[] args) {
+        LoginUI login = new LoginUI();
+        if (login.wasLoginSuccessful()) {
+            JPlotController controller = JPlotController.getInstance();
+            controller.init();
+        }
+    }
+
 }
