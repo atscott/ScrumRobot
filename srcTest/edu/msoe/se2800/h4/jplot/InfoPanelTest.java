a193153291c293a00e16fd6f01853d6557dbe813
Josh Ault <josh.ault3@gmail.com>
2013-01-24 23:55:52 -0600

    infopanel testing partial

--- srcTest/edu/msoe/se2800/h4/jplot/InfoPanelTest.java
+++ srcTest/edu/msoe/se2800/h4/jplot/InfoPanelTest.java
@@ -1,5 +1,33 @@
 package edu.msoe.se2800.h4.jplot;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import edu.msoe.se2800.h4.jplot.InfoPanel;
+
 public class InfoPanelTest {
+	
+	private InfoPanel infoPanel;
+	
+	@BeforeClass
+    public void setupGridTesting() {
+        infoPanel = new InfoPanel();
+    }
+	
+	@Test
+	public void testInitSubviews() {
+		infoPanel.initSubviews();
+	}
+
+	/**
+	 * ensuring that adding a point adds the correct point
+	 */
+	/*@Test(dependsOnMethods = {"testNullPath"})
+	public void testAddPoint() {
+		g.addPoint(new JPoint(1,2));
+		Assert.assertEquals(g.getPathPoints().size(), 1);
+		Assert.assertEquals(g.getPathPoints().get(0).x, 1);
+		Assert.assertEquals(g.getPathPoints().get(0).y, 2);
+	}*/
 
 }
