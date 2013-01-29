7ab51ea7288f351c1ee1367ff9f20d5d642d48ce
Andrew Scott <atscott01@gmail.com>
2013-01-29 12:36:43 -0600

    added back files in new package

---
+++ srcTest/edu/msoe/se2800/h4/administrationFeatures/DatabaseConnectionTest.java
@@ -0,0 +1,97 @@
+package edu.msoe.se2800.h4.administrationFeatures;
+
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * User: scottat
+ * Date: 1/21/13
+ * Time: 10:43 AM
+ */
+@Test(groups={"requiresSequential"})
+public class DatabaseConnectionTest {
+
+    @BeforeMethod
+    public void setupDB(){
+        DatabaseConnection.getInstance().tryConnect(DatabaseConnection.getInstance().DB_NAME);
+    }
+
+    @Test
+    public void testBadDB() {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        assertEquals(db.tryConnect("lkwjerlksj.slekjr"), false);
+    }
+
+    @Test
+    public void testValidDB(){
+        assertTrue(DatabaseConnection.getInstance().tryConnect(DatabaseConnection.getInstance().DB_NAME));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testValidUser() throws IOException {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        boolean retVal = db.ValidateUser("admin", "admin");
+        assertEquals(retVal, true);
+    }
+
+    @Test
+    public void testInvalidUser() throws IOException {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        boolean retVal = db.ValidateUser("lksjdflkjwelrkj", "kwjerlkjwerlkjwer");
+        assertEquals(retVal, false);
+    }
+
+    @Test
+    public void testValidUserBadPass() throws IOException {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        boolean retVal = db.ValidateUser("admin", "kwjerlkjwerlkjwer");
+        assertEquals(retVal, false);
+    }
+
+    @Test
+    public void getOtherUsersWhichIsEmpty() throws IOException {
+        List<String> users = DatabaseConnection.getInstance().getUsernamesWithRole(DatabaseConnection.UserTypes.OTHER);
+        assertTrue(users.size() == 0);
+    }
+
+    @Test
+    public void getAdminUsers() throws IOException{
+        List<String> users = DatabaseConnection.getInstance().getUsernamesWithRole(DatabaseConnection.UserTypes.ADMIN);
+        //really, the only thing we know for sure is that the admin user is in the list
+        assertTrue(users.contains("admin"));
+    }
+
+    //TODO tests for deleteUser
+    //TODO tests for addUser
+
+    //TODO tests for changeUserInfo
+    @Test
+    public void changeAdminPasswordTest() throws IOException {
+//        DatabaseConnection db = DatabaseConnection.getInstance();
+//        db.addUser("qazwsxedcrfv", "test", DatabaseConnection.UserTypes.ADMIN);
+//        db.changeUserInfo("qazwsxedcrfv", "ddd", DatabaseConnection.UserTypes.ADMIN);
+//        //verify you can log in with new credentials
+//        boolean retVal = db.ValidateUser("qazwsxedcrfv", "ddd");
+//        assertEquals(retVal, true);
+//        db.deleteUser("qazwsxedcrfv");
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        db.changeUserInfo("admin", "ddd", DatabaseConnection.UserTypes.ADMIN);
+        //verify you can log in with new credentials
+        boolean retVal = db.ValidateUser("admin", "ddd");
+        assertEquals(retVal, true);
+        //change back the password
+        db.changeUserInfo("admin", "admin", DatabaseConnection.UserTypes.ADMIN);
+        retVal = db.ValidateUser("admin", "admin");
+        assertEquals(retVal, true);
+    }
+}
