03bb76f65a229650391bdbba7adacdf851cfa756
atscott <atscott01@gmail.com>
2013-02-12 16:39:27 -0600

    major refractor of the followRoute method. This definitely needs to be tested

--- src/edu/msoe/se2800/h4/RobotControllerLejos.java
+++ src/edu/msoe/se2800/h4/RobotControllerLejos.java
@@ -26,14 +26,16 @@ public class RobotControllerLejos implements IRobotController {
     /**
      * The Navigator is used to follow a path or by point.
      */
-    private static Navigator nav;
+    private Navigator nav;
     /**
      * The path to be followed by the robot.
      */
-    private static Path path;
+    private Path path;
 
     private boolean stopRequested = false;
     private boolean singleStep;
+    private boolean isReverse = false;
+    private boolean returningToBeginning = false;
 
     /**
      * The constructor instantiates the pilot and navigator. Single step is false.
@@ -53,28 +55,41 @@ public class RobotControllerLejos implements IRobotController {
     public void setPath(Path path) {
         this.path = path;
         nav.setPath(path);
-        System.out.println("here");
     }
 
-    /**
-     * Follow the route
-     */
-    @Override
     public void followRoute() {
         this.stopRequested = false;
+
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 if (!stopRequested) {
-                    forward();
-                    reverse();
+                    if (nav.pathCompleted()) {
+                        //toggle the state of the return
+                        returningToBeginning = !returningToBeginning;
+
+                        //if we are returning to beginning of the path and isReverse is true, then
+                        //navigation should procede in a forward manner
+                        if (returningToBeginning && isReverse) {
+                            pilot = new DifferentialPilot(2, 7, Motor.A, Motor.B, false);
+
+                        } else if (returningToBeginning && !isReverse) {
+                            pilot = new DifferentialPilot(2, 7, Motor.A, Motor.B, true);
+                        } else {
+                            //if we are not returning to the beginning, then navigation should proceed
+                            //in the manner defined by the reverse option
+                            pilot = new DifferentialPilot(2, 7, Motor.A, Motor.B, isReverse);
+                        }
+                        nav = new Navigator(pilot);
+                    }
+                    nav.followPath();
                     nav.waitForStop();
                     // This has to be reset since path is reset in navigator after the followPath is called
-                    for (int i = 0; i < path.size(); i++) {
-                        nav.addWaypoint(path.get(i));
+                    if (nav.pathCompleted()) {
+                        for (int i = 0; i < path.size(); i++) {
+                            nav.addWaypoint(path.get(i));
+                        }
                     }
-                    System.out.println(nav.getPath().size());
-                    System.out.println(path.size());
 
                     //only loop if this is not single step mode
                     if (!RobotControllerLejos.this.singleStep) {
@@ -86,6 +101,39 @@ public class RobotControllerLejos implements IRobotController {
         t.start();
     }
 
+//    /**
+//     * Follow the route
+//     */
+//    @Override
+//    public void followRoute() {
+//        this.stopRequested = false;
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                if (!stopRequested) {
+//                    pilot = new DifferentialPilot(2, 7, Motor.A, Motor.B, false);
+//                    nav = new Navigator(pilot);
+//
+//                    forward();
+//                    reverse();
+//                    nav.waitForStop();
+//                    // This has to be reset since path is reset in navigator after the followPath is called
+//                    for (int i = 0; i < path.size(); i++) {
+//                        nav.addWaypoint(path.get(i));
+//                    }
+//                    System.out.println(nav.getPath().size());
+//                    System.out.println(path.size());
+//
+//                    //only loop if this is not single step mode
+//                    if (!RobotControllerLejos.this.singleStep) {
+//                        this.run();
+//                    }
+//                }
+//            }
+//        });
+//        t.start();
+//    }
+
     @Override
     public void addWaypoint(Waypoint wp) {
         nav.addWaypoint(wp);
@@ -95,19 +143,6 @@ public class RobotControllerLejos implements IRobotController {
     @Override
     public Path getPath() {
         return path;
-    }
-
-    public static void forward() {
-        nav.followPath();
-
-    }
-
-    public static void reverse() {
-        for (int i = path.size() - 2; i > -1; i--) {
-            nav.goTo(path.get(i));
-
-        }
-        nav.goTo(0, 0);
     }
 
     @Override
@@ -148,6 +183,7 @@ public class RobotControllerLejos implements IRobotController {
 
     @Override
     public void setReverse(boolean isReverse) {
+        this.isReverse = isReverse;
         pilot = new DifferentialPilot(2, 7, Motor.A, Motor.B, isReverse);
         nav = new Navigator(pilot);
     }
