08b160a3bf1d74cabeb2b73dc7be60ab3ade3755
Brewcrewfan10 <koenigj@msoe.edu>
2013-02-12 10:34:16 -0600

    Stop function

--- src/edu/msoe/se2800/h4/control/RobotDemoApp.java
+++ src/edu/msoe/se2800/h4/control/RobotDemoApp.java
@@ -1,7 +1,12 @@
 package edu.msoe.se2800.h4.control;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 import edu.msoe.se2800.h4.RobotControllerLejos;
 
@@ -25,8 +30,24 @@ public class RobotDemoApp {
 	
 	static RobotControllerLejos test = new RobotControllerLejos();
 	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		JButton stop = new JButton("stop");
+		stop.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				test.stop();
+				
+			}
+			
+		});
+		frame.add(stop);
+		frame.setVisible(true);
 		Path path = new Path();
 		path.add(new Waypoint(6,-6));
+		path.add(new Waypoint(10,10));
+		path.add(new Waypoint(2,2));
 		test.setPath(path);
 		test.followRoute();
 	}
