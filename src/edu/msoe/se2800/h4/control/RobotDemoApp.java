df4ca0adb2591d0b77168858d7c449f81497904b
Brewcrewfan10 <koenigj@msoe.edu>
2013-02-11 22:04:27 -0600

    Not needed anymore
    These classes are not needed anymore, but just in case I won't delete
    the entire classes yet.

--- src/edu/msoe/se2800/h4/control/RobotDemoApp.java
+++ src/edu/msoe/se2800/h4/control/RobotDemoApp.java
@@ -20,34 +20,5 @@ import lejos.robotics.pathfinding.Path;
  *
  */
 public class RobotDemoApp {
-//	
-//	static int x;
-//	static int y;
-//	static double length;
-//	static double angle;
-//	
-	//private static DifferentialPilot pilot = new DifferentialPilot(2.215, 2.125, Motor.B, Motor.C, false);
-	
-	//private List<JPoint> points;
-	
-	/**
-	 * @param args not used
-	 */
-	public static void main(String[] args) {
-		Robot kevin = new Robot();
-		Path path = new Path();
-		path.add(new Waypoint(-2,-2));
-		//path.add(new Waypoint(5,5));
-		path.add(new Waypoint(-1,1));
-		kevin.setPoints(path);
-		kevin.travelPath();
-	}
-	/*
-	public void setPoints(Path path){
-		points = path.getPoints();
-	}
-	*/
-	
-	
-	
+
 }
