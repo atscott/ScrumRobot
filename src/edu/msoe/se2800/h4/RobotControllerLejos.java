0a4590383d4fb8d43f8301d73ed33cbecb449d79
Brewcrewfan10 <koenigj@msoe.edu>
2013-02-12 11:11:55 -0600

    Commenting and robot class deletion

--- src/edu/msoe/se2800/h4/RobotControllerLejos.java
+++ src/edu/msoe/se2800/h4/RobotControllerLejos.java
@@ -12,27 +12,51 @@ import lejos.robotics.navigation.Pose;
 import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.Path;
 
+/**
+ * Provides the basic functions for controlling the lejos robot.
+ * @author koenigj
+ *
+ */
 public class RobotControllerLejos implements IRobotController {
-	private DifferentialPilot pilot;
+	private boolean stop = false;
+	/**
+	 * The pilot class is set to 2 cm wheel diameter and 7 cm between the wheels
+	 */
+	private static DifferentialPilot pilot;
+	/**
+	 * The Navigator is used to follow a path or by point.
+	 */
 	private static Navigator nav;
+	/**
+	 * The path to be followed by the robot.
+	 */
 	private static Path path;
-	
+	/**
+	 * The constructor instantiates the pilot and navigator. Single step is false.
+	 */
 	public RobotControllerLejos(){
 		path = new Path();
 		pilot = new DifferentialPilot(2,7,Motor.A,Motor.B);
 		nav  = new Navigator(pilot);
 		nav.singleStep(false);	
 	}
+	/**
+	 * Set the path to be followed.
+	 */
 	@Override
 	public void setPath(Path path){
 		this.path = path;
 		nav.setPath(path);
 	}
+	/**
+	 * Follow the route 
+	 */
 	@Override
 	public void followRoute(){
-		forward();
-		reverse();
-		nav.waitForStop();
+			forward();
+			reverse();
+			nav.waitForStop();
+		
 	}
 	@Override
 	public void addWaypoint(Waypoint wp){
